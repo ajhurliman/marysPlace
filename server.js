6177956a65777347c54444b53ef7506b17bb5ca3
ajHurliman <james.hurliman@gmail.com>
2016-10-19 16:56:47 -0700

    updated soap client to function in prod

--- server.js
+++ server.js
@@ -33,55 +33,82 @@ var queryOptions = {
     headers: {
       'Content-Type': 'text/xml',
       'charset': 'UTF-8',
-      'JSESSIONID': cookie
+      'JSESSIONID': ''
     },
     body: '<?xml version="1.0" encoding="UTF-8"?> <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:tns="etapestryAPI/service" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"> <SOAP-ENV:Body> <tns:getQueryResultStats xmlns:tns="etapestryAPI/service"> <String_1 xsi:type="xsd:string">Base</String_1> <String_2 xsi:type="xsd:string">ncsoTotal</String_2> </tns:getQueryResultStats> </SOAP-ENV:Body> </SOAP-ENV:Envelope>'
   };
 
-var cookie;
 var totalRaised;
 var testCounter = 0;
 
 request.post(options , function(err, res, body) {
-  if (!err && res.statusCode == 200) {
-    console.log(body);
-    console.log(JSON.stringify(res, 0, 2));
-    var cookies = res.headers['set-cookie'][0];
-    cookies = cookies.split(';');
-    cookie = cookies[0].split('=')[1];
-    console.log(cookie);
-    requestJournalEntries();
-  }
+  login(err, res, body);
 });
 
 var interval = setInterval(function() {
   request.post(options , function(err, res, body) {
-    if (!err && res.statusCode == 200) {
-      console.log(body);
-      console.log(JSON.stringify(res, 0, 2));
-
-      if (res.headers['set-cookie']) {
-        var cookies = res.headers['set-cookie'][0];
-        cookies = cookies.split(';');
-        cookie = cookies[0].split('=')[1];
-        console.log(cookie);
-      }
-      
-      requestJournalEntries();
-    }
+    login(err, res, body);
   });
 
 // }, 3000);
 }, 600000);
 
+function login(err, res, body) {
+  var parsedBody;
+  var redirectUrl;
+
+  if (!err && res && res.statusCode == 200) {
+    // console.log(body);
+    console.log(JSON.stringify(res, 0, 2));
+
+    if (res.headers['set-cookie']) {
+      var cookies = res.headers['set-cookie'][0];
+      cookies = cookies.split(';');
+      queryOptions.headers.JSESSIONID = cookies[0].split('=')[1];
+      console.log('cookie set: ', queryOptions.headers.JSESSIONID);
+    }
+
+    parsedBody = xml2js(res.body, function(error, resultJson) {
+      console.log(util.inspect(resultJson, false, null));
+
+      if (resultJson['env:Envelope'] && 
+          resultJson['env:Envelope']['env:Body'] && 
+          resultJson['env:Envelope']['env:Body'][0] &&
+          resultJson['env:Envelope']['env:Body'][0]['ns0:loginResponse'] && 
+          resultJson['env:Envelope']['env:Body'][0]['ns0:loginResponse'][0] &&
+          resultJson['env:Envelope']['env:Body'][0]['ns0:loginResponse'][0]['result'] && 
+          resultJson['env:Envelope']['env:Body'][0]['ns0:loginResponse'][0]['result'][0] && 
+          resultJson['env:Envelope']['env:Body'][0]['ns0:loginResponse'][0]['result'][0]['_']) {
+            redirectUrl = resultJson['env:Envelope']['env:Body'][0]['ns0:loginResponse'][0]['result'][0]['_'];
+            queryOptions.url = redirectUrl;
+            options.url = redirectUrl;
+            console.log('url redirected to: ', redirectUrl);
+            
+            request.post(options , function(newErr, newRes, newBody) {
+              login(newErr, newRes, newBody);
+            });
+      } else {
+        requestJournalEntries();  
+      }
+    });
+
+    
+  }
+}
+
 function requestJournalEntries() {
+  console.dir('    BEGIN QUERY');
+  console.dir(queryOptions);
+  console.dir('    END QUERY');
   request.post(queryOptions, function(err, res, body) {
+    console.log(res.body);
     if (!err && res.statusCode == 200) {
+      console.log('no error');  
       var parsedJson = xml2js(res.body, function(err, resultJson) {
-        // console.log(util.inspect(resultJson, false, null));
+        console.log(util.inspect(resultJson, false, null));
         totalRaised = resultJson['env:Envelope']['env:Body'][0]['ns0:QueryResultStats'][0]['gifted'][0]['_'];
-        // console.log(totalRaised);
-        testCounter++;
+        console.log(totalRaised);
+        // testCounter++;
       });
     } else if (err) {
       console.log(err)
@@ -95,8 +122,8 @@ server.set('port', process.env.PORT || 3000);
 server.use(express.static( process.env.PWD + '/build'));
 
 server.get('/getTotal', function(req, res) {
-  res.json({total: totalRaised}); //remove before flight
-  // res.json({total: testCounter});
+  res.json({total: totalRaised}); 
+  // res.json({total: testCounter}); //remove before flight
 });
 
 // var options = {
